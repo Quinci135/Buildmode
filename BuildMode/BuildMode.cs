39ce400f9252e08a562df95fadc3bc2cd380ca62
Quinci135 <quinci135@gmail.com>
2020-07-20 01:11:04 -0700

    Fix indents

--- BuildMode/BuildMode.cs
+++ BuildMode/BuildMode.cs
@@ -26,8 +26,8 @@ namespace BuildMode
 		public override string Name
 		{
 			get { return "BuildMode"; }
-        }
-        Timer Timer = new Timer(1000);
+		}
+	        Timer Timer = new Timer(1000);
 		public override Version Version
 		{
 			get { return Assembly.GetExecutingAssembly().GetName().Version; }
@@ -94,25 +94,25 @@ namespace BuildMode
 					NetMessage.SendData((int)PacketTypes.WorldInfo, i);
 					if (plr.statLife < plr.statLifeMax && !plr.dead)
 					{
-                        tsplr.Heal(600); //It's basically godmode anyways
+                        			tsplr.Heal(600); //It's basically godmode anyways
 						//tsplr.Heal(plr.statLifeMax - plr.statLife);
 						//plr.statLife = plr.statLifeMax;
 					}
-                    tsplr.SetBuff(1, Int16.MaxValue); //Obsidian Skin
-                    tsplr.SetBuff(3, Int16.MaxValue); // Swiftness
-					tsplr.SetBuff(11, Int16.MaxValue); // Shine
-					tsplr.SetBuff(12, Int16.MaxValue); // Night owl
-                                                        //Removed panic, issues of unobtainable buffs tsplr.SetBuff(63, Int16.MaxValue); // Panic
-                    tsplr.SetBuff(104, Int16.MaxValue); // Mining
-					tsplr.SetBuff(107, Int16.MaxValue); // Builder
-                    tsplr.SetBuff(113, Int16.MaxValue); // Lifeforce
-                    tsplr.SetBuff(104, Int16.MaxValue); // Mining
-                    tsplr.SetBuff(107, Int16.MaxValue); // Builder
-                    tsplr.SetBuff(113, Int16.MaxValue); // Lifeforce
-                    tsplr.SetBuff(207, Int16.MaxValue); // Exquisitely Stuffed aka well fed t3
+					    tsplr.SetBuff(1, Int16.MaxValue); //Obsidian Skin
+					    tsplr.SetBuff(3, Int16.MaxValue); // Swiftness
+					    tsplr.SetBuff(11, Int16.MaxValue); // Shine
+					    tsplr.SetBuff(12, Int16.MaxValue); // Night owl
+					//Removed panic, issues of unobtainable buffs tsplr.SetBuff(63, Int16.MaxValue); // Panic
+					    tsplr.SetBuff(104, Int16.MaxValue); // Mining
+					    tsplr.SetBuff(107, Int16.MaxValue); // Builder
+					    tsplr.SetBuff(113, Int16.MaxValue); // Lifeforce
+					    tsplr.SetBuff(104, Int16.MaxValue); // Mining
+					    tsplr.SetBuff(107, Int16.MaxValue); // Builder
+					    tsplr.SetBuff(113, Int16.MaxValue); // Lifeforce
+					    tsplr.SetBuff(207, Int16.MaxValue); // Exquisitely Stuffed aka well fed t3
 
-                }
-            }
+				}
+			}
 		}
 		void OnGetData(GetDataEventArgs e)
 		{
@@ -124,24 +124,24 @@ namespace BuildMode
 				switch (e.MsgID)
 				{
 					case PacketTypes.PlayerHurtV2:
-                        using (MemoryStream data = new MemoryStream(e.Msg.readBuffer, e.Index, e.Length))
-                        {
-                            data.ReadByte();
-                            PlayerDeathReason.FromReader(new BinaryReader(data));
-                            int damage = data.ReadInt16();
-                            tsplr.Heal((int)Terraria.Main.CalculateDamagePlayersTake(damage, plr.statDefense) + 2);
-                        }
+                       				 using (MemoryStream data = new MemoryStream(e.Msg.readBuffer, e.Index, e.Length))
+							{
+							    data.ReadByte();
+							    PlayerDeathReason.FromReader(new BinaryReader(data));
+							    int damage = data.ReadInt16();
+							    tsplr.Heal((int)Terraria.Main.CalculateDamagePlayersTake(damage, plr.statDefense) + 2);
+							}
 						break;
 					case PacketTypes.Teleport:
-                        if ((e.Msg.readBuffer[3] == 0))
-                        {
-                            List<int> buffs = new List<int>(plr.buffType);
-                            if (buffs.Contains(Terraria.ID.BuffID.ChaosState) && plr.inventory[plr.selectedItem].netID == Terraria.ID.ItemID.RodofDiscord) //rod 1326
-                            {
-                                tsplr.Heal(plr.statLifeMax2 / 7);
-                            }
-                        }
-                        break;
+						if ((e.Msg.readBuffer[3] == 0))
+						{
+						    List<int> buffs = new List<int>(plr.buffType);
+						    if (buffs.Contains(Terraria.ID.BuffID.ChaosState) && plr.inventory[plr.selectedItem].netID == Terraria.ID.ItemID.RodofDiscord) //rod 1326
+						    {
+							tsplr.Heal(plr.statLifeMax2 / 7);
+						    }
+						}
+                        			break;
 					case PacketTypes.PaintTile:
 					case PacketTypes.PaintWall://0%
 						{
@@ -160,58 +160,58 @@ namespace BuildMode
 								tsplr.GiveItem(lastItem.type, lastItem.maxStack + 1 - count);
 						}
 						break;
-                    case PacketTypes.Tile://1%
-                        {
+					case PacketTypes.Tile://1%
+						{
 							int count = 0;
 							int type = e.Msg.readBuffer[e.Index];
-                            switch (type)
-                            {
-                                case 1: //PlaceTile
-                                case 3: //PlaceWall
-                                case 21: //ReplaceTile
-                                case 22: //ReplaceWall
-                                    if (plr.inventory[plr.selectedItem].type != Terraria.ID.ItemID.StaffofRegrowth) //230
-                                    {
-                                        int tile = e.Msg.readBuffer[e.Index + 5];
-                                        if (tsplr.SelectedItem.tileWand > 0)
-                                            tile = tsplr.SelectedItem.tileWand;
-                                        Item lastItem = null;
-                                        foreach (Item i in plr.inventory)
-                                        {
-                                            if (i.createTile == tile || i.createWall == tile)
-                                            {
-                                                lastItem = i;
-                                                count += i.stack;
-                                            }
-                                        }
-                                        if (count <= 10 && lastItem != null)
-                                            tsplr.GiveItem(lastItem.type, lastItem.maxStack + 1 - count);
-                                    }
-                                    break;
-                                    // Placing wires
-                                case 5:  //Red
-                                case 10: //Blue
-                                case 12: //Green
-                                case 16: //Yellow
-                                    foreach (Item i in plr.inventory)
-                                    {
-                                        if (i.type == Terraria.ID.ItemID.Wire) //530
-                                            count += i.stack;
-                                    }
-                                    if (count <= 10)
-                                        tsplr.GiveItem(Terraria.ID.ItemID.Wire, 1000 - count);
-                                    break;
+						    	switch (type)
+							{
+								case 1: //PlaceTile
+								case 3: //PlaceWall
+								case 21: //ReplaceTile
+								case 22: //ReplaceWall
+								    if (plr.inventory[plr.selectedItem].type != Terraria.ID.ItemID.StaffofRegrowth) //230
+								    {
+									int tile = e.Msg.readBuffer[e.Index + 5];
+									if (tsplr.SelectedItem.tileWand > 0)
+									    tile = tsplr.SelectedItem.tileWand;
+									Item lastItem = null;
+									foreach (Item i in plr.inventory)
+									{
+									    if (i.createTile == tile || i.createWall == tile)
+									    {
+										lastItem = i;
+										count += i.stack;
+									    }
+									}
+									if (count <= 10 && lastItem != null)
+									    tsplr.GiveItem(lastItem.type, lastItem.maxStack + 1 - count);
+								    }
+								    break;
+								    // Placing wires
+								case 5:  //Red
+								case 10: //Blue
+								case 12: //Green
+								case 16: //Yellow
+								    foreach (Item i in plr.inventory)
+								    {
+									if (i.type == Terraria.ID.ItemID.Wire) //530
+									    count += i.stack;
+								    }
+								    if (count <= 10)
+									tsplr.GiveItem(Terraria.ID.ItemID.Wire, 1000 - count);
+								    break;
 
-                                case 8: //Place Actuator
-                                    foreach (Item i in plr.inventory)
-                                    {
-                                        if (i.type == Terraria.ID.ItemID.Actuator) //849
-                                            count += i.stack;
-                                    }
-                                    if (count <= 10)
-                                        tsplr.GiveItem(Terraria.ID.ItemID.Actuator, 1000 - count);
-                                    break;
-                            }
+								case 8: //Place Actuator
+								    foreach (Item i in plr.inventory)
+								    {
+									if (i.type == Terraria.ID.ItemID.Actuator) //849
+									    count += i.stack;
+								    }
+								    if (count <= 10)
+									tsplr.GiveItem(Terraria.ID.ItemID.Actuator, 1000 - count);
+								    break;
+						    	}
 						}
 						break;
 				}
@@ -254,13 +254,13 @@ namespace BuildMode
 						writer.Write(build ? 27000 : (int)Main.time);
 					}
 					break;
-                case (byte)PacketTypes.MassWireOperationPay:
-                    TSPlayer tsplr = TShock.Players[e.Buffer[7]];
-                    if (tsplr.GetData<bool>("buildmode"))
-                    {
-                        e.Handled = true; //Will never decrement wires/actuators in inventory
-                    }
-                    break;
+				case (byte)PacketTypes.MassWireOperationPay:
+				    TSPlayer tsplr = TShock.Players[e.Buffer[7]];
+				    if (tsplr.GetData<bool>("buildmode"))
+				    {
+					e.Handled = true; //Will never decrement wires/actuators in inventory
+				    }
+				    break;
 				case (byte)PacketTypes.NpcUpdate: //23
 					NPC npc = Main.npc[BitConverter.ToInt16(e.Buffer, 3)];
 					if (!npc.friendly)
@@ -272,7 +272,7 @@ namespace BuildMode
 				case (byte)PacketTypes.ProjectileNew: //27
 					short id = BitConverter.ToInt16(e.Buffer, 3);
 					int owner = e.Buffer[21];
-                    Projectile proj = Main.projectile[TShock.Utils.SearchProjectile(id, owner)];
+                    			Projectile proj = Main.projectile[TShock.Utils.SearchProjectile(id, owner)];
 					if (!proj.friendly)
 						Buffer.BlockCopy(BitConverter.GetBytes((short)(build ? 0 : proj.type)), 0, e.Buffer, 22, 2);
 					break;
@@ -291,7 +291,7 @@ namespace BuildMode
 
 			e.Player.SendSuccessMessage((e.Player.GetData<bool>("buildmode") ? "En" : "Dis") + "abled build mode.");
 			// Time
-            e.Player.SendData(PacketTypes.WorldInfo);
+            		e.Player.SendData(PacketTypes.WorldInfo);
 			// NPCs
 			for (int i = 0; i < 200; i++)
 			{
